37f4abf423cd8f26a994236150530109722a3f4c
Laura Nee <43449217+llnee@users.noreply.github.com>
2020-01-05 13:25:14 -0500

    Update phonebook.py
    deleting unnecessary comments

--- phonebook.py
+++ phonebook.py
@@ -1,34 +1,22 @@
-#Name: Laura Nee
-#phonebook.py
-
-#Purpose: to create a phonebook using specially designed classes
-#Certification of Authenticity:
-#   I certify that this lab is entirely my own work.
-
 class phoneBookEntry:
-    #properties: name, phone number
     def __init__(self, name, phoneNumber):
-        #name & num are both str's
         if type(name) == str and type(phoneNumber) == str:
-            if len(phoneNumber) == 10: #phone nums have 10 digits
+            if len(phoneNumber) == 10:
                 self.name = name
                 self.number = phoneNumber
-        else: #set defaults
+        else: #defaults
             self.name = "John Smith"
             self.number = "9999999999"
 
-    #getters
     def getName(self):
         return self.name
     def getPhone(self):
         return self.number
 
-    #setter
     def setPhone(self, phoneNumber):
         if len(phoneNumber) == 10: #phone nums have 10 digits
             self.number = phoneNumber
 
-    #str method
     def __str__(self):
         formattedNum = "(" + self.number[0:3] + ")" + self.number[3:
                                             6] + "-" + self.number[6:]
@@ -36,29 +24,19 @@ class phoneBookEntry:
 
 class Phonebook:
     def __init__(self):
-        #entries is a list of phoneBookEntry objs
         self.phoneBook = []
-
-        #defaults
         self.isSorted = False
 
-    def addEntry(self, name, phoneNumber): #V
-        #create an entry using the phoneBookEntry class
+    def addEntry(self, name, phoneNumber):
         entry = phoneBookEntry(name, phoneNumber)
-        #append that entry to the phonebook class
         self.phoneBook.append(entry)
 
     def sort(self):
         self.isSorted = True
-
-        #selection sort
         minPos = 0
         for front in range(len(self.phoneBook) - 1):
             minPos = front
             for i in range(minPos + 1, len(self.phoneBook)):
-                #if the firstLetter of phoneBook[i] is earlier
-                #than the firstLetter of phoneBook[minPos],
-                # then the actual minPos is at i
                 if ord(self.phoneBook[i].getName()[0]) < ord(self.phoneBook
                         [minPos].getName()[0]):
                     minPos = i
@@ -67,16 +45,12 @@ class Phonebook:
             self.phoneBook[minPos] = self.phoneBook[front]
             self.phoneBook[front] = lowest
 
-                
-    def findPhoneNumber(self, name): #returns phoneNumber
-        #create list of sorted names from phoneBook
+    def findPhoneNumber(self, name):
         namesInPhonebook = []
         for entry in self.phoneBook:
             Name = entry.getName()
             namesInPhonebook.append(Name)
-                
-        if self.isSorted: #binary search
-            #search list of names for the specified name
+        if self.isSorted: #perform binary search
             low = 0
             high = len(namesInPhonebook)
             mid = (low + high) // 2
@@ -88,15 +62,14 @@ class Phonebook:
                 mid = (low + high) // 2
             if low > high: #name not in phoneBook
                 mid = -1
-        else: #not sorted, linear search
+        else: #not sorted, perform linear search
             for i in range(len(namesInPhonebook)):
                 if name == namesInPhoneBook[i]:
                     mid = i
                 else:
                     mid = -1
-
         if mid == -1:
-            return ""
+            return "----------"
         else:
             return self.phoneBook[mid].getPhone()
 
@@ -108,9 +81,9 @@ class Phonebook:
 
 
 def main():
-    #valid entry that prints a single phoneBookEntry obj
     ent1 = phoneBookEntry("Jane", "8435555555")
     print(ent1)
+    
     #invalid entries that trigger default values
     ent2 = phoneBookEntry(7, "8435555555")
     print(ent2)
@@ -131,7 +104,7 @@ def main():
 
     #find phoneNumber of someone in the book
     print("Bob's phone number is", myPhonebook.findPhoneNumber("Bob"))
-    #find phoneNumber of someone not in the book, returns empty str
+    #find phoneNumber of someone not in the book
     print("Susan's phone number is", myPhonebook.findPhoneNumber("Susan"))
     
 
